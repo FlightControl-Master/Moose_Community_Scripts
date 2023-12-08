b54ee5e6911f569658d2a11a2fc977d20a632f42
Thomas <72444570+Applevangelist@users.noreply.github.com>
2023-12-08 09:37:57 +0100

    Update WW2GCI_SRS.lua

--- Scripts/WW2GCI_SRS.lua
+++ Scripts/WW2GCI_SRS.lua
@@ -10,6 +10,9 @@ ww2gci:SetCheckTime(20)
 local gcidraw = ZONE:New("Controlled Zone"):DrawZone(-1,{1,0,0},nil,{1,0,0},0.1,2,true)
 local knownbaddies = SET_GROUP:New()
 local baddiesseen = {}
+local minheight = 500 --ft
+local thresheight = 90 -- 10% chance to find a low flying group
+local thresblur = 75 -- 25% chance to escape the radar overall
 
 MESSAGE.SetMSRS(MSRS.path,MSRS.port,MSRS.google,38.4,radio.modulation.AM,nil,nil,MSRS.Voices.Google.Standard.de_DE_Standard_D,coalition.side.RED,1,"RLM",RP)
 
@@ -25,9 +28,22 @@ function Announce(Baddy)
   MESSAGE:New(text,15,"RLM"):ToSRSRed():ToRed()
 end
 
+--- Create some radar blur - doesn't take into account further radar sweeps
+-- and changes in AGL over time.
+function RadarBlur(Group)
+  local found = true
+  local group = Group -- Wrapper.Group#GROUP
+  local AGL = UTILS.MetersToFeet(group:GetAltitude(true)) -- get AGL in feet
+  local fheight = math.floor(math.random(1,10000)/100)
+  local fblur = math.floor(math.random(1,10000)/100)
+  if AGL <= minheight and fheight > thresheight then found = false end
+  if fblur > thresblur then found = false end
+  return found
+end
+
 function ww2gci:OnAfterEnteredZone(From,Event,To,Controllable)
   local group = Controllable -- Wrapper.Group#GROUP
-  if group and group:IsAlive() and (not baddiesseen[group:GetName()]) then
+  if group and group:IsAlive() and (not baddiesseen[group:GetName()]) and RadarBlur(group) then
     local text = 'Achtung! An alle Einheiten! Neuer Feindkontakt!'
     MESSAGE:New(text,15,"RLM"):ToSRSRed():ToRed()
     Announce(group)
